ced3fba030468727918cf82ef3d7c93b860dfb7d
KaiserSakhi <kaisersakhi>
2020-08-03 11:29:35 +0530

    modified a little"

--- src/com/kaisersakhi/Main.java
+++ src/com/kaisersakhi/Main.java
@@ -1,21 +1,36 @@
 package com.kaisersakhi;
 import com.kaisersakhi.linkedlist.LinkedList;
 import com.kaisersakhi.queue.Queue;
+import com.kaisersakhi.trees.BinaryTree;
+
 //owner : kaiser sakhi
+
+/*
+    Note : This code is not written by a CS professional but a CS Grad. i hosted this code on github because i wannted to get
+            taste of git , the code is totally written in simple way possible . if you're learning data structures and algorithms ,
+            this might be useful for you.
+            :this code is highly infulanced by {  Abdul Bari (Great Teacher) }
+                    link of his youtube channel : https://www.youtube.com/channel/UCZCFT11CWBi3MHNlGf019nw
+
+             if you find this code helpful in anyway than probally you should star it.
+
+             im working on python also side by side - learning Python ;)
+
+ */
 public class Main {
 
     public static void main(String[] args) {
 
-        queues();
-
+//        queues();
+        binaryTree();
     }
 
     static void queues(){
-        Queue<Integer> queue = new Queue<>();
-        queue.enqueue(10);
-        queue.enqueue(2);
-        queue.enqueue(20);
-        queue.enqueue(13);
+        Queue<Double> queue = new Queue<>();
+        queue.enqueue(10.90786533);
+        queue.enqueue(2.23);
+        queue.enqueue(450.3);
+        queue.enqueue(13.22222);
         queue.display();
         System.out.println("Dequeued " + queue.dequeue());
     }
@@ -25,5 +40,17 @@ public class Main {
         list.add(34);
         list.add(9);
         list.display();
+    }
+
+    static void binaryTree(){
+        var tree = new BinaryTree<Integer>();
+
+        tree.add(1);
+        tree.add(2);
+        tree.add(3);
+        tree.add(4);
+
+        tree.preOrder();
+
     }
 }
---
+++ src/com/kaisersakhi/trees/Node.java
@@ -0,0 +1,4 @@
+package com.kaisersakhi.trees;
+
+public class Node {
+}
